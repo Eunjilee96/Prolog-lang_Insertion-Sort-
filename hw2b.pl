d84fb858ca86606e83f106acc318a851d7dc0043
Lee Eunji (BlockChain) <31514949+Eunjilee96@users.noreply.github.com>
2022-04-28 22:20:52 +0900

    Update hw2b.pl

--- hw2b.pl
+++ hw2b.pl
@@ -1,3 +1,21 @@
+n_queens(N, Qs) :-
+        length(Qs, N),
+        Qs ins 1..N,
+        safe_queens(Qs).
+
+safe_queens([]).
+safe_queens([Q|Qs]) :-
+        safe_queens(Qs, Q, 1),
+        safe_queens(Qs).
+
+safe_queens([], _, _).
+safe_queens([Q|Qs], Q0, D0) :-
+        Q0 #\= Q,
+        abs(Q0 - Q) #\= D0,
+        D1 #= D0 + 1,
+        safe_queens(Qs, Q0, D1).
+      
+      
 show(N, Options) :-
         N #> 0,
         n_queens(N, Qs),
@@ -8,3 +26,5 @@ show(N, Options) :-
         get_single_char(_),
         false.
 show(_, _ ) :- halt.
+
+
