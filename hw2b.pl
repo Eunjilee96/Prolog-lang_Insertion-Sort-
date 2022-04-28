872872ecbe48cb4886e5e79acf88a4e0985ccd88
Lee Eunji (BlockChain) <31514949+Eunjilee96@users.noreply.github.com>
2022-04-28 22:20:03 +0900

    Update hw2b.pl

--- hw2b.pl
+++ hw2b.pl
@@ -1,1 +1,10 @@
-hw2b.pl
+show(N, Options) :-
+        N #> 0,
+        n_queens(N, Qs),
+        phrase(postscript, Ps),
+        format("~s ~w init\n", [Ps,N]),
+        animate(Qs),
+        labeling(Options, Qs),
+        get_single_char(_),
+        false.
+show(_, _ ) :- halt.
